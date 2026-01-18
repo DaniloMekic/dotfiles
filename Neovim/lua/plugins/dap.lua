b6102774a8b2140e43e8ce481656b3da8220f2cd
DaniloMekic <danilo.mekic@gmail.com>
2026-01-18 23:00:56 +0100

    Add DAP keybindings.

--- Neovim/lua/plugins/dap.lua
+++ Neovim/lua/plugins/dap.lua
@@ -1,12 +1,22 @@
+---@type LazySpec
 return {
     "rcarriga/nvim-dap-ui",
     opts = {},
+    keys = {
+        { "<Leader>du", function() require("dapui").toggle() end, desc = "DAP UI" },
+        { "<Leader>de", function() require("dapui").eval() end, desc = "DAP Eval" },
+    },
     dependencies = {
         {
             "mfussenegger/nvim-dap",
+            ---@type LazyKeysSpec[]
             keys = {
-                { "<Leader>dt", function() require("dap").toggle_breakpoint() end, desc = "Toggle breakpoint" },
-                { "<Leader>dc", function () require("dap").continue() end, desc = "Continue" }
+                { "<F5>", function() require("dap").continue() end, desc = "Debugging: Continue" },
+                { "<F9>", function() require("dap").toggle_breakpoint() end, desc = "Debugging: Toggle breakpoint" },
+                { "<F10>", function() require("dap").step_over() end, desc = "Debugging: Step Over" },
+                { "<F11>", function() require("dap").step_into() end, desc = "Debugging: Step Into" },
+                { "<F12>", function() require("dap").step_out() end, desc = "Debugging: Step Out" },
+                { "<Leader>dt", function() require("dap").terminate() end, desc = "Debugging: Terminate" },
             }
         },
         "nvim-neotest/nvim-nio",
