f505d778f7857ac57b35a3921da044aa9ea0adec
DaniloMekic <danilo.mekic.web@gmail.com>
2026-04-30 06:21:51 +0200

    Add Harper, language server for grammar.

--- Neovim/plugin/02_mason-lspconfig.lua
+++ Neovim/plugin/02_mason-lspconfig.lua
@@ -1,30 +1,31 @@
 require("mason-lspconfig").setup({
-        ---@type string[]
-        ensure_installed = {
-            -- Valid LSP Names: github.com/neovim/nvim-lspconfig/tree/master/lsp
-            "awk_ls",                  -- AWK
-            "basedpyright",            -- Python LSP
-            "bashls",                  -- Bash
-            "clangd",                  -- C, C++
-            "cssls",                   -- CSS
-            "docker_language_server",  -- Docker
-            "html",                    -- HTML
-            "jdtls",                   -- Java
-            "jsonls",                  -- JSON
-            "lemminx",                 -- XML
-            "lua_ls",                  -- Lua
-            "markdown_oxide",          -- Markdown
-            "postgres_lsp",            -- PostgreSQL
-            "rust_analyzer",           -- Rust
-            "sqls",                    -- SQL
-            "taplo",                   -- TOML
-            "texlab",                  -- LaTeX
-            "ts_ls",                   -- TypeScript
-            "vimls",                   -- VimScript
-            "yamlls",                  -- YAML
-        },
-        ---@type boolean | string[] | { exclude: string[] }
-        automatic_enable = {
-            exclude = { "jdtls" } -- See `nvim/ftplugin/java.lua`
-        }
+    ---@type string[]
+    ensure_installed = {
+        -- Valid LSP Names: github.com/neovim/nvim-lspconfig/tree/master/lsp
+        "awk_ls", -- AWK
+        "basedpyright", -- Python LSP
+        "bashls", -- Bash
+        "clangd", -- C, C++
+        "cssls", -- CSS
+        "docker_language_server", -- Docker
+        "harper_ls", -- Grammar
+        "html", -- HTML
+        "jdtls", -- Java
+        "jsonls", -- JSON
+        "lemminx", -- XML
+        "lua_ls", -- Lua
+        "markdown_oxide", -- Markdown
+        "postgres_lsp", -- PostgreSQL
+        "rust_analyzer", -- Rust
+        "sqls", -- SQL
+        "taplo", -- TOML
+        "texlab", -- LaTeX
+        "ts_ls", -- TypeScript
+        "vimls", -- VimScript
+        "yamlls", -- YAML
+    },
+    ---@type boolean | string[] | { exclude: string[] }
+    automatic_enable = {
+        exclude = { "jdtls" }, -- See `nvim/ftplugin/java.lua`
+    },
 })
