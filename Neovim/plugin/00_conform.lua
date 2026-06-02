96af989cee44d1ff7122c95f15e2b6f17dcfaba6
DaniloMekic <danilo.mekic.web@gmail.com>
2026-06-02 11:33:18 +0200

    Configure Stylua to prefer single quotes.

--- Neovim/plugin/00_conform.lua
+++ Neovim/plugin/00_conform.lua
@@ -1,31 +1,31 @@
 ---@module "conform"
-require("conform").setup({
+require('conform').setup({
     ---@type conform.setupOpts
     -- Formatters are automatically installed with Mason via mason-conform plugin
     formatters_by_ft = {
         -- clangd LSP embeds clang-format, a code formatter for C/C++/C# and other languages
         -- Styling options can be controlled with .clang-format file
         -- https://clang.llvm.org/docs/ClangFormatStyleOptions.html
-        c = { lsp_format = "prefer" },
-        cpp = { lsp_format = "prefer" },
-        groovy = { "npm-groovy-lint" },
-        java = { "google-java-format", lsp_format = "fallback" },
-        javascript = { "prettierd", "prettier" },
-        json = { "jq" },
-        lua = { "stylua" },
-        python = { "isort", "black" },
-        rust = { "rustfmt", lsp_format = "fallback" },
-        sh = { "shfmt", lsp_format = "fallback" },
-        tex = { "tex-fmt" },
+        c = { lsp_format = 'prefer' },
+        cpp = { lsp_format = 'prefer' },
+        groovy = { 'npm-groovy-lint' },
+        java = { 'google-java-format', lsp_format = 'fallback' },
+        javascript = { 'prettierd', 'prettier' },
+        json = { 'jq' },
+        lua = { 'stylua' },
+        python = { 'isort', 'black' },
+        rust = { 'rustfmt', lsp_format = 'fallback' },
+        sh = { 'shfmt', lsp_format = 'fallback' },
+        tex = { 'tex-fmt' },
     },
 
     formatters = {
-        ["google-java-format"] = {
+        ['google-java-format'] = {
             -- Use 4 space indents per Android Open Source Project (AOSP), instead of 2 per Google Java Style Guide
-            prepend_args = { "--aosp" },
+            prepend_args = { '--aosp' },
         },
         stylua = {
-            prepend_args = { "--indent-type", "spaces" },
+            prepend_args = { '--indent-type', 'Spaces', '--quote-style', 'AutoPreferSingle' },
         },
     },
 
@@ -33,7 +33,7 @@ require("conform").setup({
     format_on_save = {
         -- These options will be passed to conform.format()
         timeout_ms = 500,
-        lsp_format = "never",
+        lsp_format = 'never',
     },
 
     stop_after_first = true,
