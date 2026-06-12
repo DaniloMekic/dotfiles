2c9bf9db83dd80990b5d79db3b3e417d04ea595b
DaniloMekic <danilo.mekic.web@gmail.com>
2026-06-12 14:19:16 +0200

    chore(neovim): better keybind description, and with capitalization

--- Neovim/plugin/00_markview.lua
+++ Neovim/plugin/00_markview.lua
@@ -1,1 +1,1 @@
-vim.keymap.set('n', '<Leader>um', '<Cmd>Markview toggle<CR>', { desc = 'Toggle Markview rendering' })
+vim.keymap.set('n', '<Leader>um', '<Cmd>Markview toggle<CR>', { desc = 'Toggle Markdown Preview (Markview)' })
